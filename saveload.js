ce71f2f050d3be3def13eb2b4b752f2658e58b33
Michael <mikeramos92@gmail.com>
2018-02-15 19:46:49 -0500

    Updating with variable from index.js

--- saveload.js
+++ saveload.js
@@ -18,23 +18,34 @@ parts of project:
 
 */
 
+//15 Feb - start building around already created variables in index.js
 const readline = require('readline');
 const fs = require('fs');
 const path = require('path');
+
+let PLAYERS = 2;
+let BOARD_SIZE = 3;
+let WIN_SEQUENCE = 3;
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-let game = {
-  players: 2,
-  win_seq: 3,
-  size: 3
+//we have to extract the data from the actual game now
+//the game variables can be extracted from the initial questions from inquirer
+//declare the variables for saving as users answer questions as a "buffer" until players either save or quit
+//still need to figure out how to store what was where on the board
+let thisGame = {
+  players: PLAYERS,
+  win_seq: WIN_SEQUENCE,
+  size: BOARD_SIZE,
+  turnCt: 6
 };
 
-let json = JSON.stringify(game);
+let json = JSON.stringify(thisGame);
 
+//saveFile needs to encode the game into the json object
 function saveFile(currentGame) {
   rl.question('Enter file name: ', (fname) => {
     fs.writeFileSync(path.join(__dirname, fname), currentGame)
@@ -42,17 +53,25 @@ function saveFile(currentGame) {
   })
 }
 
+//loadFile reads and reopens
+//call the printGameBoard function through this?
 function loadFile() {
   rl.question('Enter file name: ', (fname) => {
     fs.readFile(path.join(__dirname, fname), 'utf-8', (err, data) => {
       if (err) {
         console.error(`An error occurred while opening ${fname}`, err);
       } else {
-        console.log(data);
+        var game = JSON.parse(data);
+        console.log(game.players);
+        console.log(game.win_seq);
+        console.log(game.size);
+        var whoseTurn = (game.turnCt % game.players) + 1;
+        console.log(`It's player ${whoseTurn}'s turn`); //can be designed to show marking instead
       }
     })
     rl.close()
   })
 }
 
+//saveFile(json);
 loadFile();
